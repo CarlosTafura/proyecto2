77cf1138dd95dc1ddc970988311995c47c07ccb9
Carlos Tafura <carlostafura@gmail.com>
2022-07-27 23:56:15 -0300

    Add carrucel and footer

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './DarkVariantExample';
+import DarkVariantExample from './DarkVariantExample';
+
+const Body = () => {
+    return (
+        <DarkVariantExample/>
+    );
+};
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NavScrollExample';
+import NavScrollExample from './NavScrollExample';
+
+const Header = () => {
+    return (
+       <NavScrollExample/>
+    );
+};
+
+export default Header;
